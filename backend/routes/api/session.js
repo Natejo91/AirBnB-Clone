64052f89f99694f3ea37ca1384cc5bcf7541cbd1
nathaniel <nathanieljortega@gmail.com>
2021-03-25 13:19:10 -0500

    setting up api routes for seesion and user

---
+++ backend/routes/api/session.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+module.exports = router;

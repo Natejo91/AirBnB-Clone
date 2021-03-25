ffdc2d24cc107d56f96a84eef79b673702725259
nathaniel <nathanieljortega@gmail.com>
2021-03-25 11:11:12 -0500

    set up backend server with one route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
@@ -6,8 +7,8 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const routes = require('./routes');
 
-const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -16,19 +17,23 @@ app.use(express.json());
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
-  }
-  // helmet helps set a variety of headers to better secure your app
-  app.use(helmet({
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(helmet({
     contentSecurityPolicy: false
-  }));
+}));
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
+// Set the _csrf token and create req.csrfToken method
+app.use(
     csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
     })
-  );
+    );
+
+app.use(routes);
+
+module.exports = app;
